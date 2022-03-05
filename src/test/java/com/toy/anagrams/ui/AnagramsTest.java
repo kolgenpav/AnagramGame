dd6b5781b10ffcacf1cd65c3bc24f9ebb47d6797
Hennadii Kolomoiets <kolgenpav@gmail.com>
2022-03-05 19:39:46 +0200

    Added Parameterized Tests support to pom.xml and refactored test to parameterized in AnagramsTest.

--- src/test/java/com/toy/anagrams/ui/AnagramsTest.java
+++ src/test/java/com/toy/anagrams/ui/AnagramsTest.java
@@ -1,21 +1,40 @@
 package com.toy.anagrams.ui;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class AnagramsTest {
 
-    @Test
-    @DisplayName("boolean check(final int awordIdx, final String aguessedWordText) test with awordIdx = 0 " +
-            "and aguessedWordText = abstraction")
-    void check() {
+    @DisplayName("Test of boolean check(final int awordIdx, final String aguessedWordText) method")
+    @ParameterizedTest(name = "{index} ==> at Idx={0} word is {1} - that is {2}")
+    @CsvSource({
+            "0, abstraction, true",
+            "1, 'abstraction', false"
+    })
+    public void check(int wordIdx, String guessedWordText, boolean expResult) {
         Anagrams anagrams = new Anagrams();
-//        int awordIdx = 0;
-        int awordIdx = 1;
-        String aguessedWordText = "abstraction";
-//        assertTrue(anagrams.check(awordIdx, aguessedWordText));
-        assertFalse(anagrams.check(awordIdx, aguessedWordText));
+        boolean result = anagrams.check(wordIdx, guessedWordText);
+        assertEquals(expResult, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringIntAndStringProvider")
+    public void checkWithArgsMethod(int wordIdx, String guessedWordText, boolean expResult) {
+        Anagrams anagrams = new Anagrams();
+        boolean result = anagrams.check(wordIdx, guessedWordText);
+        assertEquals(expResult, result);
+    }
+
+    static Stream<Arguments> stringIntAndStringProvider() {
+        return Stream.of(arguments(0, "abstraction", true),
+                arguments(1, "abstraction", false));
     }
 }
