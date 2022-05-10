8b932e6eaf376ceff28ceab38af5e76587b40118
Hennadii Kolomoiets <kolgenpav@gmail.com>
2022-05-10 20:41:09 +0300

    Different source using demonstration for CountLettersParameterizedTest were added.

--- src/test/java/com/toy/anagrams/examples/CountLettersParameterizedTest.java
+++ src/test/java/com/toy/anagrams/examples/CountLettersParameterizedTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 //@Disabled
 class CountLettersParameterizedTest {
 
-    @DisplayName("Test of int count(String str) method with MC/DC parameters determined")
+    @DisplayName("Parameterized test of int count(String str) method with CSVSource")
     @ParameterizedTest
     @CsvSource({
             "r|, 1",
@@ -19,8 +26,32 @@ class CountLettersParameterizedTest {
             "s, 1",
             "r, 1"
     })
-    void count(String word, int count) {
+    void countCSVSource(String word, int count) {
         int words = new CountLetters().count(word);
         Assertions.assertEquals(count, words);
+    }
+
+    @DisplayName("Parameterized test of int count(String str) method with CSVFileSource")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/params.csv", numLinesToSkip = 1)  //csv file have to located in resources folder
+    void countCSVFileSource(String word, int count) {
+        int words = new CountLetters().count(word);
+        Assertions.assertEquals(count, words);
+    }
+
+    @DisplayName("Parameterized test of int count(String str) method with MethodSource")
+    @ParameterizedTest
+    @MethodSource("paramsProvider")
+    void countMethodSource(String word, int count) {
+        int words = new CountLetters().count(word);
+        Assertions.assertEquals(count, words);
+    }
+
+    static Stream<Arguments> paramsProvider() {
+        return Stream.of(arguments("r|", 1),
+                arguments("s|", 1),
+                arguments("a|", 0),
+                arguments("s", 1),
+                arguments("r", 1));
     }
 }
