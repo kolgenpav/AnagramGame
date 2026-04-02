c508dbd9a38f045bcf425259153106306f2d4ff1
kolgenpav <kolgenpav@gmail.com>
2026-04-02 12:10:32 +0300

    SpotBug suggestion about possible NullPointerException and excessive null check for HashSet.

--- src/main/java/com/toy/anagrams/examples/NullPointerProblem.java
+++ src/main/java/com/toy/anagrams/examples/NullPointerProblem.java
@@ -14,15 +14,20 @@ public final class NullPointerProblem {
     /**
      * Entry point.
      *
-     * @param args
+     * @param args command line args don't used
      */
     public static void main(final String[] args) {
         Set<Person> persons = new HashSet<>();
+        persons.add(new Person("John"));
         for (Person p : persons) {
-            if (p != null) {
-                System.out.println(p.getName());
-            }
-            System.out.println(p.getName() + " не знайдено");
+            /*SpotBugs will report about excessive null check.
+              because the loop will not be executed if the set is empty. */
+//            if (p != null) {
+//                System.out.println(p.getName());
+//            }
+            /*SpotBugs will report about possible null pointer exception. */
+//            System.out.println(p.getName()  + " не знайдено");
+            System.out.println((p != null ? p.getName() : "Person") + " не знайдено");
         }
 
         double a = 1000.0000000000001;
@@ -30,7 +35,7 @@ public final class NullPointerProblem {
     }
 
     static class Person {
-        private String name;
+        private final String name;
 
         Person(final String aname) {
             this.name = aname;
